36f6ba5bdd01d009af4cfa4d026f3231fa056f77
Ronaiza Cardoso <ronaizacard@gmail.com>
2018-11-05 19:38:35 +0000

    Updates

---
+++ precache-manifest.b55cd4ffcf60cbdd4063f06829b471df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed11ee52079052b13b6b",
+    "url": "/currency-exchange/static/js/runtime~main.ed11ee52.js"
+  },
+  {
+    "revision": "92eb0cf4cb2caf12f897",
+    "url": "/currency-exchange/static/js/main.92eb0cf4.chunk.js"
+  },
+  {
+    "revision": "fb6e1c4887ad5917bbe8",
+    "url": "/currency-exchange/static/js/1.fb6e1c48.chunk.js"
+  },
+  {
+    "revision": "92eb0cf4cb2caf12f897",
+    "url": "/currency-exchange/static/css/main.1c74d169.chunk.css"
+  },
+  {
+    "revision": "37c41d55bd236ec91aa5c0550a21ce84",
+    "url": "/currency-exchange/index.html"
+  }
+];
